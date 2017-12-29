d6d9e72646854776d5a419f2b8023230d33323ee
Jyothirmai <Jyo.techm@gmail.com>
2017-12-29 11:33:51 +0530

    3rd commit

--- calculator/src/main/java/com/example/App.java
+++ calculator/src/main/java/com/example/App.java
@@ -6,8 +6,9 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public int add(int a, int b){
+		return a+b;
+	}
+	
 }
